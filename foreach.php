79f072071e75b8007495bd5d8e15f823da18343b
jay nichols <jay_nichols_3@hotmail.com>
2016-07-05 14:43:18 -0500

    added scalar

--- foreach.php
+++ foreach.php
@@ -4,20 +4,10 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 
 foreach($things as $thing) {
-	if (is_int($thing)) {
-		echo "{$thing} is an integer\n";
-	} elseif (is_float($thing)) {
-		echo "{$thing} is a float\n";
-	} elseif (is_bool($thing)) {
-		echo "{$thing} is a boolean\n";
-	} elseif (is_array($thing)) {
-	  	echo "{$thing} is an array\n";
-	} elseif (is_null($thing)) {
-		echo "{$thing} is null\n";
-	} elseif (is_string($thing)) {
-		echo "{$thing} is a string\n";
+	if (is_scalar($thing)) {
+		echo "{$thing} is scalar.\n";
 	} else {
-		echo "It is none of these.";
+		echo "These aren't scalar.\n";
 	}
 
 }
