26692cf9d4482163fbe7c3556d2cf566dddf88c4
jay nichols <jay_nichols_3@hotmail.com>
2016-07-05 13:29:47 -0500

    added argument to incrementor

--- for.php
+++ for.php
@@ -5,6 +5,10 @@ $firstNumber = trim(fgets(STDIN));
 fwrite(STDOUT, 'Type a higher number. ');
 $secondNumber = trim(fgets(STDIN));
 
-for ($i = $firstNumber; $i <= $secondNumber; $i++) {
-	fwrite(STDOUT, "{$i}\n");
+if ($argc === 2) {
+
+	for ($i = $firstNumber; $i <= $secondNumber; $i += $argv[1]) {
+		fwrite(STDOUT, "{$i}\n");
+	}
+
 }
