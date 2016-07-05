2e18505b093b401b7b662fbf36a1557ca1218069
jay nichols <jay_nichols_3@hotmail.com>
2016-07-05 14:54:44 -0500

    printed selected indices

--- foreach.php
+++ foreach.php
@@ -4,10 +4,13 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 
 foreach($things as $thing) {
-	if (is_scalar($thing)) {
-		echo "{$thing} is scalar.\n";
-	} else {
-		echo "These aren't scalar.\n";
+	if (is_string($thing)) {
+		echo "{$thing}\n";
+	} elseif (is_int($thing)) {
+		echo "{$thing}\n";
+	} elseif (is_array($thing)) {
+		echo "{$thing}\n";
 	}
+
 
 }
