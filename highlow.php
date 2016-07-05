e598bb3af2c0f2b4d0c9b64de0d2500ca636cdbf
jay nichols <jay_nichols_3@hotmail.com>
2016-07-05 11:53:16 -0500

    added exit code

--- highlow.php
+++ highlow.php
@@ -2,30 +2,31 @@
 
 if ($argc == 3) {
 
-$randomNumber = mt_rand($argv[1], $argv[2]);
+	$randomNumber = mt_rand($argv[1], $argv[2]);
 
 
 
 
-$counter = 0;
+	$counter = 0;
 
-do {
-	fwrite(STDOUT, 'Guess a number between 1 and 100. ');
-	$number = trim(fgets(STDIN));
+	do {
+		fwrite(STDOUT, 'Guess a number between 1 and 100. ');
+		$number = trim(fgets(STDIN));
 
-	if ($number < $randomNumber) {
-		fwrite(STDOUT, "Higher\n");
-	} elseif ($number > $randomNumber) {
-		fwrite(STDOUT, "Lower\n");	
-	} else {
-		fwrite(STDOUT, "Correct\n");
-	}
-	$counter++;
-} while ($number !== $randomNumber);
+		if ($number < $randomNumber) {
+			fwrite(STDOUT, "Higher\n");
+		} elseif ($number > $randomNumber) {
+			fwrite(STDOUT, "Lower\n");	
+		} else {
+			fwrite(STDOUT, "Correct\n");
+		}
+		$counter++;
+	} while ($number !== $randomNumber);
 
-fwrite(STDOUT, "You guessed $counter times.");
+	fwrite(STDOUT, "You guessed $counter times.");
 
 }
+	exit(0);
 
 
 
