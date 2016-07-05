bdd4b8f1604fe332d410753d276007b7822f6a06
jay nichols <jay_nichols_3@hotmail.com>
2016-07-05 10:30:41 -0500

    completed program/added arguments

--- highlow.php
+++ highlow.php
@@ -1,17 +1,33 @@
 <?php 
 
-$randomNumber = mt_rand(1, 100);
+if ($argc == 3) {
 
-fwrite(STDOUT, 'Guess a number between 1 and 100. ');
-
-
-$number = fgets(STDIN);
+$randomNumber = mt_rand($argv[1], $argv[2]);
 
 
-if ($number < $randomNumber) {
-	fwrite(STDOUT, "Higher\n");
-} elseif ($number > $randomNumber) {
-	fwrite(STDOUT, "Lower\n");	
-} else {
-	fwrite(STDOUT, "That's the number.\n");
+
+
+$counter = 0;
+
+do {
+	fwrite(STDOUT, 'Guess a number between 1 and 100. ');
+	$number = trim(fgets(STDIN));
+
+	if ($number < $randomNumber) {
+		fwrite(STDOUT, "Higher\n");
+	} elseif ($number > $randomNumber) {
+		fwrite(STDOUT, "Lower\n");	
+	} else {
+		fwrite(STDOUT, "Correct\n");
+	}
+	$counter++;
+} while ($number !== $randomNumber);
+
+fwrite(STDOUT, "You guessed $counter times.");
+
 }
+
+
+
+
+
