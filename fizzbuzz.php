1b18e75d92d797b4abdc1e4be43b7dcbdee1c157
jay nichols <jay_nichols_3@hotmail.com>
2016-07-05 12:22:37 -0500

    corrected code

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,13 +1,11 @@
 <?php
 
 for ($i = 0; $i <= 100; $i++) {
-	if ($i % 3 == 0) {
+	if ($i % 3 == 0 && $i % 5 != 0) {
 		fwrite(STDOUT, "FIZZ\n");
-	}
-	if ($i % 5 == 0) {
+	} elseif ($i % 5 == 0 && $i % 3 != 0) {
 		fwrite(STDOUT, "BUZZ\n");
-	} 
-	if ($i % 3 == 0 && $i % 5 == 0) {
+	} elseif ($i % 3 == 0 && $i % 5 == 0) {
 		fwrite(STDOUT, "FIZZBUZZ\n");
 	} else {
 		fwrite(STDOUT, "{$i}\n");
