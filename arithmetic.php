70eaa3676a14a274f647ec94b644141404db2272
jay nichols <jay_nichols_3@hotmail.com>
2016-07-06 11:15:00 -0500

    initial commit / testing functions

---
+++ arithmetic.php
@@ -0,0 +1,28 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+}
+
+// Add code to test your functions here
+
+echo add(3, 4) . PHP_EOL;
+echo subtract(3, 4) . PHP_EOL;
+echo multiply(3, 4) . PHP_EOL;
+echo divide(3, 4) . PHP_EOL;
