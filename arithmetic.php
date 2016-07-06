f59fb27321bdbc84c4c3fa07dc8e0ffa0823b1fe
jay nichols <jay_nichols_3@hotmail.com>
2016-07-06 11:21:04 -0500

    added modulus function

--- arithmetic.php
+++ arithmetic.php
@@ -26,3 +26,10 @@ echo add(3, 4) . PHP_EOL;
 echo subtract(3, 4) . PHP_EOL;
 echo multiply(3, 4) . PHP_EOL;
 echo divide(3, 4) . PHP_EOL;
+
+
+function modulus($a, $b) {
+	return $a % $b;
+}
+
+echo modulus(12, 5) . PHP_EOL;
