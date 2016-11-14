ad991181c06c0b31cfd4260d730852908181888b
jay nichols <jay_nichols_3@hotmail.com>
2016-11-14 10:56:32 -0600

    formatted phone number and fixed array structure

--- contact_parser.php
+++ contact_parser.php
@@ -1,15 +1,36 @@
 <?php
 
+function formatPhoneNumber($numberString) 
+{
+	$firstThree = substr($numberString, 0, 3);
+	$middleThree = substr($numberString, 3, 3);
+	$lastThree = substr($numberString, 6, 4);
+
+	return "{$firstThree}-{$middleThree}-{$lastThree}";
+}
+
+
 function parseContacts($filename)
 {
+	$contacts = [];
 	$handle = fopen($filename, 'r');
-	$contents = fread($handle, filesize($filename));
-	$contacts = explode("|", $contents);
+	$contents = trim(fread($handle, filesize($filename)));
 	fclose($handle);
 
-	// todo - read file and parse contacts
+	$contents = explode("\n", $contents);
+
+	foreach ($contents as $line) {
+		$contact = [];
+		$line = explode("|", $line);
+		$contact['name'] = $line[0];
+		$contact['number'] = formatPhoneNumber($line[1]);
+
+		$contacts[] = $contact;
+	}
 
 	return $contacts;
+
+	
 }
 
 var_dump(parseContacts('contacts.txt'));
